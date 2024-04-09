d3cd256fed224ccfaab6b3d57246a1220baa3df0
Martin Sedlacek <martas.sedlacek@seznam.cz>
2024-04-09 08:07:48 +0200

    Implemented FileController

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -3,15 +3,59 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\HeaderUtils;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class FileController extends AbstractController
 {
 
     #[Route('/file/small')]
-    public function serveSmallAction()
+    public function serveSmallAction(): StreamedResponse
     {
-        return $this->json("heyyyya");
+        return $this->makeDownloadResponse('/storage/video/01.mp4', 'video/mp4', 'downloaded small video.mp4');
     }
+
+    #[Route('/file/large')]
+    public function serveLargeAction(): StreamedResponse
+    {
+        return $this->makeDownloadResponse('/storage/video/02.mp4', 'video/mp4', 'downloaded large video.mp4');
+
+    }
+
+    #[Route('/file/medium')]
+    public function serveMediumAction(): StreamedResponse
+    {
+        return $this->makeDownloadResponse('/storage/img/01.png', 'image/png', 'downloaded medium image.png');
+    }
+
+
+    protected function makeDownloadResponse(string $path, string $mimeType, string $filename)
+    {
+        $projectDir = $this->getParameter('kernel.project_dir');
+
+        $response = new StreamedResponse(function() use ($projectDir, $path) {
+            $fileStream = fopen($projectDir . $path, 'r');
+
+            // Output the file content in chunks
+            while (!feof($fileStream)) {
+                echo fread($fileStream, 1024); // Adjust chunk size as needed
+                flush();
+            }
+
+            fclose($fileStream);
+        });
+        $response->headers->set('Content-Type', $mimeType);
+        $disposition = HeaderUtils::makeDisposition(
+            HeaderUtils::DISPOSITION_ATTACHMENT,
+            $filename,
+        );
+        $response->headers->set('Content-Disposition', $disposition);
+
+
+        return $response;
+    }
+
+
 
 }
